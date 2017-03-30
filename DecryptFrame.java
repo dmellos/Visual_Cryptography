8672b917055470f1be490cbe9a5020834bcc9a89
SELWYN DMELLO <dmelloselwyn@gmail.com>
2017-03-29 22:10:47 -0400

    Update DecryptFrame.java

--- DecryptFrame.java
+++ DecryptFrame.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
